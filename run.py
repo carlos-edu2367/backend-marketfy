f9684cfa3e92946c42a6530deaa15fc38f2f8386
carlos-edu2367 <contato.carlosedu2367@gmail.com>
2026-01-23 18:43:44 -0300

    fix. run railway

--- run.py
+++ run.py
@@ -3,9 +3,16 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # Adiciona a pasta 'app' ao caminho do Python
+    # Adiciona a pasta 'app' ao PYTHONPATH
     sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
-    
-    # Inicia o servidor
-    print("🚀 Iniciando SGM Marketfy Backend...")
-    uvicorn.run("infra.web.main:app", host="127.0.0.1", port=8000, reload=True)
+
+    port = int(os.environ.get("PORT", 8000))
+
+    print(f"🚀 Iniciando SGM Marketfy Backend na porta {port}...")
+
+    uvicorn.run(
+        "infra.web.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False
+    )
